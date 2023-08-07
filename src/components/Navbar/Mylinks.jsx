513c100ec0fdc6b3a6bd1aa030c38d14dd2a5dff
Jaco-Miras <miras.jaco@gmail.com>
2023-08-07 23:58:15 +0800

    initial commit

---
+++ src/components/Navbar/Mylinks.jsx
@@ -0,0 +1,57 @@
+export const links = [
+  {
+    name: "Municipality",
+    submenu: true,
+    sublinks: [
+      {
+        sublink: [
+          { name: "History", link: "/" },
+          { name: "General Information", link: "/" },
+          { name: "Municipal Officials", link: "/" },
+          { name: "LGU Departments", link: "/" },
+          { name: "Awards", link: "/" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "S.E.R.B.I.S.Y.O",
+    submenu: true,
+    sublinks: [
+      {
+        sublink: [
+          { name: "Social and Health Services", link: "/" },
+          { name: "Education Assistance Program", link: "/" },
+          { name: "Revenue Generation Program", link: "/" },
+          {
+            name: "Barangay Empowerment and Peace & Order Management",
+            link: "/",
+          },
+          { name: "Infrastructure Development Projects", link: "/" },
+          { name: "Youth Development Program", link: "/" },
+          {
+            name: "Organizational Environment and Human Resource Management Program",
+            link: "/",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Transparancy",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "Bids and Awards", link: "/" },
+          { name: "Full Disclosure", link: "/" },
+          { name: "Job Opportunity", link: "/" },
+          { name: "Finance", link: "/" },
+          { name: "Citizen's Charter", link: "/" },
+          { name: "LGSF Quarterly Report", link: "/" },
+        ],
+      },
+    ],
+  },
+];
